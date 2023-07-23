a6bb016949755065c9edf5a4bd23ef5883974fc9
[Ibrahim Saber] <[ibrahemsab.8907@gmail.com]>
2023-07-23 11:14:35 +0300

    Updating The DIO Driver & Solving some errors
    dated, 23/7/2023

--- COTS/MCAL/DIO/DIO_Config.h
+++ COTS/MCAL/DIO/DIO_Config.h
@@ -3,30 +3,6 @@
 #ifndef DIO_CONFIG_H
 #define DIO_CONFIG_H
 
-/*
-/* Please Select the configuration of the initial state of the PORTs Initial direction:
- * 																	1- DIO_u8_INITIAL_OUTPUT
- * 																	2- DIO_u8_INITIAL_INPUT
- */
-#define DIO_u8_PORTA_INIT_DIR    DIO_u8_INITIAL_INPUT
-#define DIO_u8_PORTB_INIT_DIR    DIO_u8_INITIAL_INPUT
-#define DIO_u8_PORTC_INIT_DIR    DIO_u8_INITIAL_INPUT
-#define DIO_u8_PORTD_INIT_DIR    DIO_u8_INITIAL_INPUT
-
-
-/*
- * Please Select the configuration of the initial state of the PORTs Initial Value:
- * 																	1- DIO_u8_OUTPUT_LOW
- * 																	2- DIO_u8_OUTPUT_HIGH
- * 																	3- DIO_u8_INPUT_FLOATING
- * 																	4- DIO_u8_INPUT_PULLUP
- */
-#define DIO_u8_PORTA_INIT_VAL   DIO_u8_INPUT_FLOATING
-#define DIO_u8_PORTB_INIT_VAL   DIO_u8_INPUT_FLOATING
-#define DIO_u8_PORTC_INIT_VAL   DIO_u8_INPUT_FLOATING
-#define DIO_u8_PORTD_INIT_VAL   DIO_u8_INPUT_FLOATING
-
-
 
 
 #endif
--- COTS/MCAL/DIO/DIO_Program.c
+++ COTS/MCAL/DIO/DIO_Program.c
@@ -20,6 +20,7 @@ void DIO_voidSetPinValue(u8 Copy_u8PortId, u8 Copy_u8PinId, u8 Copy_u8Value)
     
     if((Copy_u8PortId <= DIO_u8_PORTD) || (Copy_u8PinId <= DIO_u8_PIN7))
     {
+
         switch(Copy_u8PortId)   
         {
             case DIO_u8_PORTA:  switch(Copy_u8Value)    
@@ -140,47 +141,22 @@ u8 DIO_u8GetPinValue(u8 Copy_u8PortId, u8 Copy_u8PinId)
 /*********************************************************************************************************/
 void DIO_voidSetPortValue(u8 Copy_u8PortId, u8 Copy_u8Value)
 {
-    
-	switch(Copy_u8Value)
+	if (Copy_u8PortId <= DIO_u8_PORTD)
 	{
-		case DIO_u8_HIGH:
+		switch(Copy_u8PortId)
 		{
-			switch(Copy_u8PortId)
-			{
-				case DIO_u8_PORTA:  DIO_u8_PORTA_REG = 0xff; break;
-				case DIO_u8_PORTB:  DIO_u8_PORTB_REG = 0xff; break;
-				case DIO_u8_PORTC:  DIO_u8_PORTC_REG = 0xff; break;
-				case DIO_u8_PORTD:  DIO_u8_PORTD_REG = 0xff; break;
-			}
-
-			break;
+			case DIO_u8_PORTA: DIO_u8_PORTA_REG = Copy_u8Value; break;
+			case DIO_u8_PORTB: DIO_u8_PORTB_REG = Copy_u8Value; break;
+			case DIO_u8_PORTC: DIO_u8_PORTC_REG = Copy_u8Value; break;
+			case DIO_u8_PORTD: DIO_u8_PORTD_REG = Copy_u8Value; break;
 		}
-
-		case DIO_u8_LOW:
-		{
-
-			switch(Copy_u8PortId)
-			{
-				case DIO_u8_PORTA:  DIO_u8_PORTA_REG = 0x00; break;
-				case DIO_u8_PORTB:  DIO_u8_PORTB_REG = 0x00; break;
-				case DIO_u8_PORTC:  DIO_u8_PORTC_REG = 0x00; break;
-				case DIO_u8_PORTD:  DIO_u8_PORTD_REG = 0x00; break;
-			}
-
-			break;
-
-		}
-
-		default:
-		{
-			case DIO_u8_PORTA:  DIO_u8_PORTA_REG = Copy_u8Value; break;
-			case DIO_u8_PORTB:  DIO_u8_PORTB_REG = Copy_u8Value; break;
-			case DIO_u8_PORTC:  DIO_u8_PORTC_REG = Copy_u8Value; break;
-			case DIO_u8_PORTD:  DIO_u8_PORTD_REG = Copy_u8Value; break;
-		}
-
 	}
-   
+
+	else
+	{
+		/*do nothing*/
+	}
+
 }
 
 
@@ -192,47 +168,21 @@ void DIO_voidSetPortValue(u8 Copy_u8PortId, u8 Copy_u8Value)
 /*************************************************************************************************************/
 void DIO_voidSetPortDirection(u8 Copy_u8PortId, u8 Copy_u8Direction)
 {
-    
-	switch(Copy_u8Direction)
+	if (Copy_u8PortId <= DIO_u8_PORTD)
 	{
-		case DIO_u8_OUTPUT:
+		switch(Copy_u8PortId)
 		{
-			switch(Copy_u8PortId)
-			{
-				case DIO_u8_PORTA:  DIO_u8_DDRA_REG =  0xff; break;
-				case DIO_u8_PORTB:  DIO_u8_DDRB_REG =  0xff; break;
-				case DIO_u8_PORTC:  DIO_u8_DDRC_REG =  0xff; break;
-				case DIO_u8_PORTD:  DIO_u8_DDRD_REG =  0xff; break;
-			}
-
-			break;
+			case DIO_u8_PORTA: DIO_u8_PORTA_REG = Copy_u8Direction; break;
+			case DIO_u8_PORTB: DIO_u8_PORTB_REG = Copy_u8Direction; break;
+			case DIO_u8_PORTC: DIO_u8_PORTC_REG = Copy_u8Direction; break;
+			case DIO_u8_PORTD: DIO_u8_PORTD_REG = Copy_u8Direction; break;
 		}
-
-		case DIO_u8_INPUT:
-		{
-
-			switch(Copy_u8PortId)
-			{
-				case DIO_u8_PORTA:  DIO_u8_DDRA_REG = 0x00; break;
-				case DIO_u8_PORTB:  DIO_u8_DDRB_REG = 0x00; break;
-				case DIO_u8_PORTC:  DIO_u8_DDRC_REG = 0x00; break;
-				case DIO_u8_PORTD:  DIO_u8_DDRD_REG = 0x00; break;
-			}
-
-			break;
-
-		}
-
-		default:
-		{
-			case DIO_u8_PORTA:  DIO_u8_DDRA_REG = Copy_u8Direction; break;
-			case DIO_u8_PORTB:  DIO_u8_DDRB_REG = Copy_u8Direction; break;
-			case DIO_u8_PORTC:  DIO_u8_DDRC_REG = Copy_u8Direction; break;
-			case DIO_u8_PORTD:  DIO_u8_DDRD_REG = Copy_u8Direction; break;
-		}
-
 	}
-   
+
+	else
+	{
+		/*do nothing*/
+	}
 
 }
 
@@ -244,160 +194,30 @@ void DIO_voidSetPortDirection(u8 Copy_u8PortId, u8 Copy_u8Direction)
 /*****************************************************************************************************************/
 u8 DIO_u8GetPortValue(u8 Copy_u8PortId)
 {
-   u8 Local_u8PortValue = 0;
 
-   switch(Copy_u8PortId)
-   {
-   	   case DIO_u8_PORTA:	Local_u8PortValue = DIO_u8_PINA_REG; break;
-   	   case DIO_u8_PORTB:	Local_u8PortValue = DIO_u8_PINB_REG; break;
-   	   case DIO_u8_PORTC:	Local_u8PortValue = DIO_u8_PINC_REG; break;
-   	   case DIO_u8_PORTD:	Local_u8PortValue = DIO_u8_PIND_REG; break;
+	u8 Local_u8PortValue = 0;
 
-   }
+	if(Copy_u8PortId <= DIO_u8_PORTD)
+	{
 
-   return Local_u8PortValue;
+
+	   switch(Copy_u8PortId)
+	   {
+		   case DIO_u8_PORTA:	Local_u8PortValue = DIO_u8_PINA_REG; break;
+		   case DIO_u8_PORTB:	Local_u8PortValue = DIO_u8_PINB_REG; break;
+		   case DIO_u8_PORTC:	Local_u8PortValue = DIO_u8_PINC_REG; break;
+		   case DIO_u8_PORTD:	Local_u8PortValue = DIO_u8_PIND_REG; break;
+
+	   }
+
+
+	}
+
+	else
+	{
+		/*Do nothing*/
+	}
+
+	return Local_u8PortValue;
+
 }
-
-
-
-
-
-
-/********************************************************************/
-/* 	This Section for the initial state configuration implementation */
-/****************************************************************** */
-
-/*Initial Direction for PORTA*/
-#if DIO_u8_PORTA_INIT_DIR == DIO_u8_INITIAL_INPUT
-DDRA = 0x00;
-
-#elif DIO_u8_PORTA_INIT_DIR == DIO_u8_INITIAL_OUTPUT
-DDRA = 0xff;
-
-#else
-#error Wrong Configuration of PORTA Initial Direction
-
-#endif
-
-
-
-/*Initial Direction for PORTB*/
-#if DIO_u8_PORTB_INIT_DIR == DIO_u8_INITIAL_INPUT
-DDRB = 0x00;
-
-#elif DIO_u8_PORTB_INIT_DIR == DIO_u8_INITIAL_OUTPUT
-DDRB = 0xff;
-
-#else
-#error Wrong Configuration of PORTA Initial Direction
-
-#endif
-
-
-
-/*Initial Direction for PORTC*/
-#if DIO_u8_PORTC_INIT_DIR == DIO_u8_INITIAL_INPUT
-DDRC = 0x00;
-
-#elif DIO_u8_PORTC_INIT_DIR == DIO_u8_INITIAL_OUTPUT
-DDRC = 0xff;
-
-#else
-#error Wrong Configuration of PORTA Initial Direction
-
-#endif
-
-
-
-/*Initial Direction for PORTD*/
-#if DIO_u8_PORTD_INIT_DIR == DIO_u8_INITIAL_INPUT
-		DDRD = 0x00;
-
-#elif DIO_u8_PORTD_INIT_DIR == DIO_u8_INITIAL_OUTPUT
-		DDRD = 0xff;
-
-#else
-#error Wrong Configuration of PORTD Initial Direction
-
-#endif
-
-
-
-
-
-/*Initial Value of PORTA*/
-#if DIO_u8_PORTA_INIT_VAL == DIO_u8_HIGH
-		PORTA = 0xff;
-
-#elif DIO_u8_PORTA_INIT_VAL == DIO_u8_LOW
-		PORTA = 0x00;
-
-#elif DIO_u8_PORTA_INIT_VAL == DIO_u8_INPUT_FLOATING
-		PORTA = 0x00
-
-#elif DIO_u8_PORTA_INIT_VAL == DIO_u8_INPUT_PULLUP
-		PORTA = 0xff;
-
-#else
-#error Wrong Configuration of the initial value of PORTA
-
-#endif
-
-/*Initial Value of PORTB*/
-#if DIO_u8_PORTB_INIT_VAL == DIO_u8_HIGH
-		PORTB = 0xff;
-
-#elif DIO_u8_PORTB_INIT_VAL == DIO_u8_LOW
-		PORTB = 0x00;
-
-#elif DIO_u8_PORTB_INIT_VAL == DIO_u8_INPUT_FLOATING
-		PORTB = 0x00
-
-#elif DIO_u8_PORTB_INIT_VAL == DIO_u8_INPUT_PULLUP
-		PORTB = 0xff;
-
-#else
-#error Wrong Configuration of the initial value of PORTB
-
-#endif
-
-
-
-/*Initial Value of PORTC*/
-#if DIO_u8_PORTC_INIT_VAL == DIO_u8_HIGH
-		PORTC = 0xff;
-
-#elif DIO_u8_PORTC_INIT_VAL == DIO_u8_LOW
-		PORTC = 0x00;
-
-#elif DIO_u8_PORTC_INIT_VAL == DIO_u8_INPUT_FLOATING
-		PORTC = 0x00
-
-#elif DIO_u8_PORTC_INIT_VAL == DIO_u8_INPUT_PULLUP
-		PORTC = 0xff;
-
-#else
-#error Wrong Configuration of the initial value of PORTC
-
-#endif
-
-
-
-
-/*Initial Value of PORTD*/
-#if DIO_u8_PORTD_INIT_VAL == DIO_u8_HIGH
-		PORTD = 0xff;
-
-#elif DIO_u8_PORTD_INIT_VAL == DIO_u8_LOW
-		PORTD = 0x00;
-
-#elif DIO_u8_PORTD_INIT_VAL == DIO_u8_INPUT_FLOATING
-		PORTD = 0x00
-
-#elif DIO_u8_PORTD_INIT_VAL == DIO_u8_INPUT_PULLUP
-		PORTD = 0xff;
-
-#else
-#error Wrong Configuration of the initial value of PORTC
-
-#endif
